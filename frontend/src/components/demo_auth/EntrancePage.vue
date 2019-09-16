685a25d66a1e5a7c035983e5640ec544a6a0b378
이석원 <blackmonkey9256@gmail.com>
2019-09-16 17:33:40 +0900

    기능: 로그인 유무에 따른 페이지 이동 처리완료

--- frontend/src/components/demo_auth/EntrancePage.vue
+++ frontend/src/components/demo_auth/EntrancePage.vue
@@ -14,7 +14,7 @@
           <v-card flat dark>
             <template v-if="item === '로그인'">
               <v-card-text>
-                <v-form ref="form" v-model="valid" lazy-validation>
+                <v-form ref="form" v-model="valid">
                   <v-text-field v-model="username" :rules="nameRules" label="Username" required></v-text-field>
                   <v-text-field
                     v-model="password"
@@ -49,20 +49,18 @@ export default {
   components: {
     "signup-card": SignUpCard
   },
-  data() {
-    return {
-      tab: null,
-      items: ["로그인", "회원가입"],
-      valid: true,
-      username: "",
-      nameRules: [v => !!v || "Username입력은 필수입니다."],
-      password: "",
-      passwordRules: [
-        v => !!v || "패스워드입력은 필수입니다.",
-        v => v.length >= 4 || "패스워드의 길이는 4글자 이상이어야 합니다."
-      ]
-    };
-  },
+  data: () => ({
+    tab: null,
+    items: ["로그인", "회원가입"],
+    valid: true,
+    username: "",
+    nameRules: [v => !!v || "Username입력은 필수입니다."],
+    password: "",
+    passwordRules: [
+      v => !!v || "패스워드입력은 필수입니다.",
+      v => v.length >= 4 || "패스워드의 길이는 4글자 이상이어야 합니다."
+    ]
+  }),
   methods: {
     login() {
       const params = {
@@ -71,12 +69,14 @@ export default {
       };
 
       if (this.$refs.form.validate()) {
+        alert("dsfsd");
         api
           .login(params)
           .then(res => {
             if (res.status === 200) {
               this.$store.state.auth.userInfo = res.data;
               this.$cookies.set("user", res.data.username, "30min");
+              this.$router.replace("/");
             }
           })
           .catch(err => {
