c55ff241e4b988a5ae58c2675bce086b71b7ae58
이석원 <blackmonkey9256@gmail.com>
2019-10-08 17:24:39 +0900

    빌드 확인됨

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,12 +2,19 @@ module.exports = {
   publicPath: '/',
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000/',
+      '^/api/': {
+        target: 'http://localhost:8000/api/',
       },
       '/static/posters': {
         target: 'http://localhost:8000/',
       },
     }
+  },
+  outputDir: "./dist/",
+  assetsDir: "static",
+  
+  async fetchPhrases() {
+    const response = await this.$http.get('/api/phrases/')
+    return response.body.data
   }
 }
